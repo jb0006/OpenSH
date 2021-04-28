29105fc54317317c072c419a13c285005c207449
Braveo <jluisbg10@gmail.com>
2021-04-27 23:32:17 -0700

    Response Structures work nicely

--- PycharmProj/main.py
+++ PycharmProj/main.py
@@ -1,6 +1,4 @@
 import speech_recognition as sr
-import pyaudio
-import wave
 from gtts import gTTS
 from os import path
 from playsound import playsound
@@ -16,26 +14,37 @@ r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)
 
-outputString = r.recognize_google(audio)
+outputString = r.recognize_google(audio).lower()
+print(outputString)
 
-try:
-    print("Recognition thinks you said " + outputString)
-except sr.UnknownValueError:
-    print("Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Recognition error; {0}".format(e))
+# try:
+#     print("Recognition thinks you said " + outputString)
+# except sr.UnknownValueError:
+#     print("Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Recognition error; {0}".format(e))
+
+import response_structure as rs
 
 responseString = ""
 
-if "your mom" in outputString:
-    responseString = "shut up fabian, you just got roasted by a computer"
-elif "I want to die" in outputString:
-    responseString = "I am going to take over the world"
-else:
-    responseString = outputString
+for res in rs.all:
+    responseString = res.Procedure(outputString)
+    if responseString is not "":
+        break
 
-tts = gTTS(responseString)
-tts.save("tts.mp3")
-playsound("tts.mp3")
+# if "your mom" in outputString:
+#     responseString = "shut up fabian, you just got roasted by a computer"
+# elif "I want to die" in outputString:
+#    responseString = "I am going to take over the world"
+# else:
+#     responseString = outputString
 
-# playsound("output.wav")
+if responseString is not "":
+    tts = gTTS(responseString)
+    tts.save("tts.mp3")
+    playsound("tts.mp3")
+
+# tts = gTTS(outputString)
+# tts.save("tts.mp3")
+# playsound("tts.mp3")
--- PycharmProj/response_structure.py
+++ PycharmProj/response_structure.py
@@ -1,5 +1,13 @@
 
-class Response:
+class Command:
     def __init__(self, user_input, response):
         self.user_input = user_input
         self.response = response
+    def Procedure(self, inputString) -> str:
+        if self.user_input in inputString:
+            return self.response
+
+all = [
+    Command("joe mama", "good question"),
+    Command("you suck", "no, please do not say that")
+]
